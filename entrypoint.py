dab15941cee22270bf0b7d0b1e2d40335222d177
Dallin Wright <5984803+dallinwright@users.noreply.github.com>
2020-06-03 15:50:23 +0200

    Dev (#8)
    * adding deps
    
    * adding deps
    
    * fixed vague error 1 message
    
    * fixed vague error 1 message
    
    Co-authored-by: Dallin Wright <dallin.wright@n26.com>

--- entrypoint.py
+++ entrypoint.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+import json
+import os
 import subprocess
 import sys
 import logging
 import argparse
+
+terraform_token_file = "/Users/dallinwright/.terraform.d/credentials.tfrc.json"
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -13,6 +17,16 @@ def status_check(rc):
     if rc != 0:
         logging.error('Exit code: {0}'.format(rc))
         sys.exit(rc)
+
+
+def write_token(token):
+    data = {'credentials': {}}
+    data['credentials']['app.terraform.io'] = {
+        "token": token
+    }
+
+    with open(terraform_token_file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 def call_os_command(command):
@@ -28,6 +42,7 @@ def call_os_command(command):
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('terratest', help='Whether we should include terratests', default='none')
+    parser.add_argument('token', help='Cloud authentication token for terraform cloud')
     args = parser.parse_args()
 
     stage = 'Terraform Format Check (terraform fmt)'
@@ -41,13 +56,17 @@ def main():
     call_os_command(['tflint'])
 
     if args.terratest == 'full':
-        stage = 'Terraform Integration Testing (terratest)'
+        logging.info('Writing auth token')
+        write_token(args.token)
+
         logging.info('Installing go dependencies')
         call_os_command(['go', 'get', '-v', 'github.com/gruntwork-io/terratest/modules/terraform'])
         call_os_command(['go', 'get', '-v', 'github.com/stretchr/testify/assert'])
 
-        logging.info('Calling {0}'.format(stage))
-        call_os_command(['go', 'test', '-v', './tests'])
+        if os.path.isfile(terraform_token_file):
+            stage = 'Terraform Integration Testing (terratest)'
+            logging.info('Calling {0}'.format(stage))
+            call_os_command(['go', 'test', '-v', './tests'])
 
     sys.exit(0)
 
